d8a1ec15255c2a9aff90f468a9c7c2fc2231c090
支云 <mr@zhiyun.com>
2026-06-18 13:47:47 +0800

    tighten webview security

--- apk/app/src/main/java/com/zhiyun/exam/MainActivity.java
+++ apk/app/src/main/java/com/zhiyun/exam/MainActivity.java
@@ -16,11 +16,8 @@ public class MainActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
-        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
